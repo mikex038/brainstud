c2eced57aa1e14173e2012bc01b5ce70828fdbd8
mikex038 <mikewendt@live.nl>
2016-11-01 16:38:33 +0100

    gg well played

--- test/_Website-FINAL.php
+++ test/_Website-FINAL.php
@@ -1,8 +1,11 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title> Website FINAL</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- Save for Web Styles ( Website FINAL.psd) -->
 <link rel="stylesheet" type="text/css" href="css/main.css">
+
 <!-- End Save for Web Styles -->
 </head>
 <body>
@@ -10,42 +13,30 @@
 <?php include('header.inc.php');
 $header = new BrainHeader();
 $header->getStart();?>
-<!-- Save for Web Slices ( Website FINAL.psd) -->
 
+
+<!-- Save for Web Slices ( Website FINAL.psd) -->
 	<section class="subsection-intro1">
 	</section>
 
-	<section class="subsection-intro2">
-	</section>
 
-	<section class="subsection-intro3">
-	</section>
+		<section class="subsection-intro2">
+		</section>
 
-	<section class="subsection-intro4">
-	</section>
+		<section class="subsection-intro3">
+		</section>
 
-	<section class="subsection-intro5">
-	</section>
+		<section class="subsection-intro4">
+		</section>
 
-	<section class="subsection-intro6">
-	</section>
+		<section class="subsection-intro5">
+		</section>
 
-	<section class="subsection-intro7">	
-	</section>
+		<section class="subsection-intro6">
+		</section>
 
-	<section class="subsection-intro8">
-	</section>
-
-	<section class="subsection-intro9">
-	</section>
-
-	<section class="subsection-intro10">
-	</section>
-
-
-<?php include('footer.inc.php');?>
-<?php $header->getEnd(); ?>
-
+		<?php include('footer.inc.php');?>
+		<?php $header->getEnd(); ?>
 
 <!-- End Save for Web Slices -->
 </body>
