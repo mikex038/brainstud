0a7c63172dc52bd09c3941bb72aa5ff3e9886582
Thijs vd Broeke <Thijs_Broeke@live.nl>
2016-11-08 11:40:39 +0100

    .
    .

--- final/js/main.js
+++ final/js/main.js
@@ -3,13 +3,9 @@ $(document).ready(function() {
     var PSV = new PhotoSphereViewer({
 	    panorama: 'images/photosphere.JPG',
 	    container: 'photosphere',
-	    navbar: 'autorotate zoom download caption fullscreen',
+	    autorotate: true,
 	    default_fov: 70,
 	    mousewheel: false,
-	    //size: {
-		 //   height: 500
-		 //   width: 400
-	    //}
 	  });
 
 
